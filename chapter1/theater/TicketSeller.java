02f3f66a9c658af8f312d40a520a1f989007f299
sejin park <seijin0722@naver.com>
2023-11-21 15:03:29 +0900

    :bulb: TicketSeller에서 Bag에 접근하는 모든 로직 -> Audience TicketSeller가 Audience인터페이스에만 의존해서 Bag에 접근할 수 없음 -> 관람객 스스로 초대장있는지 가장 확인하고, 없을 경우 구매하는 로직

--- chapter1/theater/TicketSeller.java
+++ chapter1/theater/TicketSeller.java
@@ -8,20 +8,7 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            // 판매원에게 맞은 티켓을 관람객의 가방안에 넣어준다.
-            Ticket ticket = ticketOffice.getTickets();
-            audience.getBag().setTicket(ticket);
-            // 초대장이 없다면 -> 이벤트 당첨되지 않은 관람객
-        } else {
-            // 판매원이 티켓을 판매한다.
-            Ticket ticket = ticketOffice.getTickets();
-            // 소극장이 관람객의 가방에서 티켓 금액 만큼 차감
-            audience.getBag().minusAmount(ticket.getFee());
-            // 매표소의 금액을 판매금액 만큼 증가시킨다
-            ticketOffice.plusAmount(ticket.getFee());
-            // 가방안에 넣어준다.
-            audience.getBag().setTicket(ticket);
-        }
+        // 판매된 금액 매표소에 증가시킴
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTickets()));
     }
 }
