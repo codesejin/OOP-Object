fe426ebc77c9959d11b1da54cf4eaeb8755bc8fc
sejin park <seijin0722@naver.com>
2023-11-21 14:48:40 +0900

    :bulb: Theater의 enter메서드에서 TicketOffice에 접근하는 모든 코드 -> TicketSeller TicketSeller에서 getTicketOffice메서드가 제거됌 -> ticketOffice의 가시성이 private이고 접근 가능한 퍼블릭 메서드가 더이상 존재하지 않음 -> 외부에서 ticketOffice에 더이상 직접 접근할 수 없음 -> 결론 : ticketOffice에서 티켓을 꺼내거나 판매 요금을 적립하는건 TickketSeller만 할 수 있음 -> Point : 개념적으로나 물리적으로 객체 내부의 세부사항을 감추는 것 : 캡슐화

--- chapter1/theater/Theater.java
+++ chapter1/theater/Theater.java
@@ -8,21 +8,6 @@ public class Theater {
     }
 
     public void enter(Audience audience) {
-        // 관람객이 가방안에 초대장이 있는지 확인 -> 이벤트 당첨자
-        if (audience.getBag().hasInvitation()) {
-            // 판매원에게 맞은 티켓을 관람객의 가방안에 넣어준다.
-            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
-            audience.getBag().setTicket(ticket);
-            // 초대장이 없다면 -> 이벤트 당첨되지 않은 관람객
-        } else {
-            // 판매원이 티켓을 판매한다.
-            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
-            // 소극장이 관람객의 가방에서 티켓 금액 만큼 차감
-            audience.getBag().minusAmount(ticket.getFee());
-            // 매표소의 금액을 판매금액 만큼 증가시킨다
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            // 가방안에 넣어준다.
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience);
     }
 }
