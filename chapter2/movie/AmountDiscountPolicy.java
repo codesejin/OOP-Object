54b7a6465d602dc511767bd77e6e14d17dfd59bf
sejin park <seijin0722@naver.com>
2023-11-28 07:05:48 +0900

    :bulb: 추상클래스와 인터페이스의 트레이드 오프 DiscountPolicy - 인터페이스, DefaultDiscountPolicy - 추상클래스

--- chapter2/movie/AmountDiscountPolicy.java
+++ chapter2/movie/AmountDiscountPolicy.java
@@ -1,6 +1,6 @@
 package chapter2.movie;
 
-public class AmountDiscountPolicy extends DiscountPolicy {
+public class AmountDiscountPolicy extends DefaultDiscountPolicy {
     private Money discountAmount;
 
     public AmountDiscountPolicy(Money discountAmount, DiscountCondition ... conditions) {
