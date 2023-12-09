e2dd02203deee5fa0a54bbebeb55c1c89174f02d
sejin park <seijin0722@naver.com>
2023-12-09 12:53:18 +0900

    ✨ 책임 할당하기 초안 문제점 : 변경에 취약한 클래스인 Discountcondition

---
+++ chapter5/movie/Customer.java
@@ -0,0 +1,4 @@
+package chapter5.movie;
+
+public class Customer {
+}
---
+++ chapter5/movie/Movie.java
@@ -0,0 +1,51 @@
+package chapter5.movie;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountCondition;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private Money discountPercent;
+    public Money calculateMovieFee(Screening screening) {
+        if (isDiscountable(screening)) {
+            return fee.minus(calculateDiscountAmount());
+        }
+        return fee;
+    }
+
+    private boolean isDiscountable(Screening screening) {
+        return discountCondition.stream()
+                .anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    private Money calculateDiscountAmount() {
+        switch (movieType) {
+            case AMOUNT_DISCOUNT:
+                return calculateAmountDiscountAmount();
+            case PERCENT_DISCOUNT:
+                return calculatePercentDiscountAmount();
+            case NONE_DISCOUNT:
+                return calculateANoneDiscountAmount();
+        }
+        throw new IllegalStateException();
+    }
+
+    private Money calculateAmountDiscountAmount() {
+        return discountAmount;
+    }
+
+    private Money calculatePercentDiscountAmount() {
+        return fee.times(discountPercent);
+    }
+
+    private Money calculateANoneDiscountAmount() {
+        return Money.ZERO;
+    }
+
+}
