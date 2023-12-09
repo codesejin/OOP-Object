a4dfae18c3db5a998808564cae03c2e305b7147d
sejin park <seijin0722@naver.com>
2023-12-09 13:28:20 +0900

    :bulb: 타입 분리하기 문제점 : DiscountCondition 자체의 응집도는 높아졌지만, 변경과 캡슐화 관점에서 설계 품질이 나빠짐

--- chapter5/movie/Movie.java
+++ chapter5/movie/Movie.java
@@ -1,51 +1,22 @@
 package chapter5.movie;
 
-import java.time.Duration;
 import java.util.List;
 
 public class Movie {
-    private String title;
-    private Duration runningTime;
-    private Money fee;
-    private List<DiscountCondition> discountCondition;
-
-    private MovieType movieType;
-    private Money discountAmount;
-    private Money discountPercent;
-    public Money calculateMovieFee(Screening screening) {
-        if (isDiscountable(screening)) {
-            return fee.minus(calculateDiscountAmount());
-        }
-        return fee;
-    }
+    private List<PeriodCondition> periodConditions;
+    private List<SequenceCondition> sequenceConditions;
 
     private boolean isDiscountable(Screening screening) {
-        return discountCondition.stream()
+        return checkPeriodConditions(screening) || checkSequenceConditions(screening);
+    }
+
+    private boolean checkPeriodConditions(Screening screening) {
+        return periodConditions.stream()
                 .anyMatch(condition -> condition.isSatisfiedBy(screening));
     }
 
-    private Money calculateDiscountAmount() {
-        switch (movieType) {
-            case AMOUNT_DISCOUNT:
-                return calculateAmountDiscountAmount();
-            case PERCENT_DISCOUNT:
-                return calculatePercentDiscountAmount();
-            case NONE_DISCOUNT:
-                return calculateANoneDiscountAmount();
-        }
-        throw new IllegalStateException();
+    private boolean checkSequenceConditions(Screening screening) {
+        return sequenceConditions.stream()
+                .anyMatch(condition -> condition.isSatisfiedBy(screening));
     }
-
-    private Money calculateAmountDiscountAmount() {
-        return discountAmount;
-    }
-
-    private Money calculatePercentDiscountAmount() {
-        return fee.times(discountPercent);
-    }
-
-    private Money calculateANoneDiscountAmount() {
-        return Money.ZERO;
-    }
-
 }
---
+++ chapter5/movie/SequenceCondition.java
@@ -0,0 +1,14 @@
+package chapter5.movie;
+
+public class SequenceCondition {
+
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+}
