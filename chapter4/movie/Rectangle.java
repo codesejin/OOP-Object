60094324eea197b9fdb90ec87d68a5123f471bad
sejin park <seijin0722@naver.com>
2023-12-12 00:26:26 +0900

    :bulb: 캡슐화를 지켜라, 사각형 예제로 알아보는 캡슐화 가시성을 private으로 설정했다고 해도 접근자와 수정자를 통해 속성을 외부로 제공한다면 캡슐화를 위반하는 것이다

---
+++ chapter4/movie/Rectangle.java
@@ -0,0 +1,54 @@
+package chapter4.movie;
+
+public class Rectangle {
+    private int left;
+    private int top;
+    private int right;
+    private int bottom;
+
+    public Rectangle(int left, int top, int right, int bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+
+    public void setRight(int right) {
+        this.right = right;
+    }
+
+    public void setBottom(int bottom) {
+        this.bottom = bottom;
+    }
+    /**
+     * 캡슐화한 로직 - 책임의 이동
+     */
+    public void enlarge(int multiple) {
+        right *= multiple;
+        bottom *= multiple;
+    }
+}
